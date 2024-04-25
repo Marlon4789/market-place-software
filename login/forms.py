0978f8ed98b49882ad37bd87c637e3a94e0b0538
Marlon4789 <cuartasm844@gmail.com>
2024-04-24 20:22:31 -0500

    creacion de alertas

--- login/forms.py
+++ login/forms.py
@@ -2,7 +2,9 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+
 class CustomUserCreationForm(UserCreationForm):
+ 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['username'].help_text = '' # Establecer la ayuda de username en vacio
--- login/views.py
+++ login/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth import logout
 from .forms import CustomUserCreationForm
 from django.contrib.auth import authenticate, login as auth_login
 from django.contrib.auth.forms import AuthenticationForm
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
@@ -21,19 +24,35 @@ def profile(request):
 
 
 def register(request):
-   data = {
-       'form': CustomUserCreationForm()
-   }
-   if request.method == 'POST':
-       user_creation_form = CustomUserCreationForm(data=request.POST)
+    if request.method == 'GET':
+        return render(request, 'registration/register.html', {"form": CustomUserCreationForm})
+    else:
+        if request.POST["password1"] == request.POST["password2"]:
+            try:
+                user = User.objects.create_user(
+                    request.POST["username"], password=request.POST["password1"])
+                user.save()
+                auth_login(request, user)
+                return redirect('profile')
+            except IntegrityError:
+                error_message = "Este usuario ya está registrado."
+                return render(request, 'registration/register.html', {"form": CustomUserCreationForm, "error": error_message})
+        else:
+            error_message = "Las contraseñas no coinciden."
+            return render(request, 'registration/register.html', {"form": CustomUserCreationForm, "error": error_message})
+#    data = {
+#        'form': CustomUserCreationForm()
+#    }
+#    if request.method == 'POST':
+#        user_creation_form = CustomUserCreationForm(data=request.POST)
        
-       if user_creation_form.is_valid():
-           user_creation_form.save()
+#        if user_creation_form.is_valid():
+#            user_creation_form.save()
 
-           user = authenticate(username=user_creation_form.cleaned_data['username'], password=user_creation_form.cleaned_data['password1'])
-           auth_login(request, user)
-           return redirect('profile')
-   return render(request, 'registration/register.html', data)
+#            user = authenticate(username=user_creation_form.cleaned_data['username'], password=user_creation_form.cleaned_data['password1'])
+#            auth_login(request, user)
+#            return redirect('profile')
+#    return render(request, 'registration/register.html', data)
 
 
 
@@ -44,7 +63,8 @@ def login_view(request):
         user = authenticate(
             request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
-            return render(request, 'registration/login.html', {"form": AuthenticationForm, "error": "Username or password is incorrect."})
+            error_message = "Nombre de usuario o contraseña incorrectos."
+            return render(request, 'registration/login.html', {"form": AuthenticationForm, "error": error_message})
 
         auth_login(request, user)
         return redirect('profile')
