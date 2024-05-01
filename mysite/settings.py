ec1daf467bb90c6230409d772a59d73470639fc2
Marlon Cuartas <cuartasm844@gmail.com>
2024-04-30 21:10:35 -0500

    Actualizar settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -142,5 +142,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "may.oficial.portafolio@gmail.com"
-EMAIL_HOST_PASSWORD = "creandoPortafolio1"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
